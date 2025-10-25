80ebc5888ab2984128dcd5dc6dadb57aca0d1018
01TB <tsitohaina.berthin@gmail.com>
2025-10-25 07:51:22 +0300

    Annotation au niveau controller

--- src/main/java/servlet/annotation/URLMapping.java
+++ src/main/java/servlet/annotation/URLMapping.java
@@ -8,6 +8,6 @@ import java.lang.annotation.ElementType;
 @Target(ElementType.METHOD)
 // L’annotation doit être conservée à l’exécution (pour être lisible par réflexion)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Route {
+public @interface URLMapping {
     String url() default "/";
 }
