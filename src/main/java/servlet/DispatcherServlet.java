e5e8243385170dbd0acbdd0f67966af32dfa658f
01TB <tsitohaina.berthin@gmail.com>
2025-11-07 16:06:06 +0300

    Finalisation

--- src/main/java/servlet/DispatcherServlet.java
+++ src/main/java/servlet/DispatcherServlet.java
@@ -43,7 +43,6 @@ public class DispatcherServlet extends HttpServlet {
             ControllerInfo info = urlMap.get(path);
             if (info != null) {
                 // Mapping trouvé : afficher infos controller/méthode
-                resp.setContentType("text/plain");
                 PrintWriter out = resp.getWriter();
                 out.println("Controller: " + info.getControllerClass().getName());
                 out.println("Method name: " + info.getMethod().getName());
@@ -54,20 +53,21 @@ public class DispatcherServlet extends HttpServlet {
                     Object controllerNewInstance = info.getControllerClass().getDeclaredConstructor().newInstance();
                     Object returnObject = methodURL.invoke(controllerNewInstance);
 
-                    // Type de retour String 
-                    if(returnObject instanceof String) {
+                    
+                    if(returnObject instanceof String) {    // Type de retour String 
+                        resp.setContentType("text/plain");
                         out.println("Retour de la méthode du controller (String) : " + returnObject);
-
-                    } else if (returnObject instanceof ModelView) {
+                    } else if (returnObject instanceof ModelView) { // Type de retour ModelView
                         ModelView mv = (ModelView) returnObject;
-                        out.println("Retour de la méthode du controller (ModelView) : " + mv.toString());
+                        RequestDispatcher dispatcher = req.getRequestDispatcher("/" + mv.getView());
+                        dispatcher.forward(req, resp);
                     } else {
                         out.println("Le type de retour de la méthode du controller n'est ni de type ModelView ni String!");
                     }
                 } catch (InstantiationException | IllegalArgumentException | NoSuchMethodException | SecurityException e) {
                     System.out.println("Erreur lors de la création de l'instance du controller : " + e.getMessage());
                 } catch (IllegalAccessException | InvocationTargetException e) {
-                    System.out.println("Erreur lors de l'invocation de la méthode ou du controller : " + e.getMessage());
+                    System.out.println("Erreur lors de l'invocation de la méthode : " + e.getMessage());
                 }
             } else {
                 // Ni statique ni mapping : 404 custom
